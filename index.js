7d891e699bde05c86cb5538be9bda6270888f22e
Maya Vera <maya@mayavera.me>
2019-11-10 22:52:58 -0500

    created basic cli wrapper for poc

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
+#!/usr/bin/env node
+
 const chalk = require('chalk');
 const semver = require('semver');
+const commander = require('commander');
 
-const lockA = require('./data/lodash.4-17-14.package-lock.json');
-const lockB = require('./data/lodash.4-17-15.package-lock.json');
+function diff (a, b) {
+  const lockA = require(`./${a}`);
+  const lockB = require(`./${b}`);
 
-const versions = {};
+  const versions = {};
 
-for (const [name, info] of Object.entries(lockA.dependencies)) {
-  versions[name] = [info, null];
-}
+  for (const [name, info] of Object.entries(lockA.dependencies)) {
+    versions[name] = [info, null];
+  }
 
-for (const [name, info] of Object.entries(lockB.dependencies)) {
-  if (versions[name]) {
-    versions[name] = [versions[name][0], info];
-  } else {
-    versions[name] = [null, info];
+  for (const [name, info] of Object.entries(lockB.dependencies)) {
+    if (versions[name]) {
+      versions[name] = [versions[name][0], info];
+    } else {
+      versions[name] = [null, info];
+    }
+  }
+
+  for (const [name, [a, b]] of Object.entries(versions)) {
+    if (!b) {
+      console.log(`${name} ${chalk.red('removed')}`);
+    } else if (!a) {
+      console.log(`${name} ${chalk.green('added')}`);
+    } else if (!semver.eq(a.version, b.version)) {
+      const color = semver.gt(a.version, b.version) ? chalk.red : chalk.green;
+
+      console.log(`${name} ${color(`${a.version} -> ${b.version}`)}`);
+    }
   }
 }
 
-for (const [name, [a, b]] of Object.entries(versions)) {
-  if (!b) {
-    console.log(`${name} ${chalk.red('removed')}`);
-  } else if (!a) {
-    console.log(`${name} ${chalk.green('added')}`);
-  } else if (!semver.eq(a.version, b.version)) {
-    const color = semver.gt(a.version, b.version) ? chalk.red : chalk.green;
-
-    console.log(`${name} ${color(`${a.version} -> ${b.version}`)}`);
-  }
-}
+commander
+  .version('0.1.0')
+  .arguments('<old> <new>')
+  .action(diff)
+  .parse(process.argv);
