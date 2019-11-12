ba97ec6587a7b52cb507f9093215457e26d75cc1
Maya Vera <maya@mayavera.me>
2019-11-12 18:00:37 -0500

    improved names, simplified data somewhat

--- index.js
+++ index.js
@@ -4,39 +4,47 @@ const chalk = require('chalk');
 const semver = require('semver');
 const commander = require('commander');
 
-function diff (a, b) {
-  const lockA = require(`./${a}`);
-  const lockB = require(`./${b}`);
+function diff (oldPath, newPath) {
+  const oldLock = require(`./${oldPath}`);
+  const newLock = require(`./${newPath}`);
 
-  const versions = {};
+  const changes = {};
 
-  for (const [name, info] of Object.entries(lockA.dependencies)) {
-    versions[name] = [info, null];
+  for (const [name, { version }] of Object.entries(oldLock.dependencies)) {
+    changes[name] = [version, null];
   }
 
-  for (const [name, info] of Object.entries(lockB.dependencies)) {
-    if (versions[name]) {
-      versions[name] = [versions[name][0], info];
+  for (const [name, { version }] of Object.entries(newLock.dependencies)) {
+    if (changes[name]) {
+      changes[name] = [changes[name][0], version];
     } else {
-      versions[name] = [null, info];
+      changes[name] = [null, version];
     }
   }
 
-  for (const [name, [a, b]] of Object.entries(versions)) {
-    if (!b) {
-      console.log(`${name} ${chalk.red('removed')}`);
-    } else if (!a) {
-      console.log(`${name} ${chalk.green('added')}`);
-    } else if (!semver.eq(a.version, b.version)) {
-      const color = semver.gt(a.version, b.version) ? chalk.red : chalk.green;
+  for (const [name, [oldVersion, newVersion]] of Object.entries(changes)) {
+    if (!oldVersion) {
 
-      console.log(`${name} ${color(`${a.version} -> ${b.version}`)}`);
+      console.log(`${name} ${chalk.red('removed')}`);
+
+    } else if (!newVersion) {
+
+      console.log(`${name} ${chalk.green('added')}`);
+    
+    } else if (!semver.eq(oldVersion, newVersion)) {
+
+      const color = semver.gt(oldVersion, newVersion)
+        ? chalk.red
+        : chalk.green;
+
+      console.log(`${name} ${color(`${oldVersion} -> ${newVersion}`)}`);
+    
     }
   }
 }
 
 commander
   .version('0.1.0')
-  .arguments('<old> <new>')
+  .arguments('<oldPath> <newPath>')
   .action(diff)
   .parse(process.argv);
