3a45f2afd2673256ccb4a31b603257f4f5f95599
Maya Vera <maya@mayavera.me>
2019-11-13 16:48:19 -0500

    created option to enable/disable colorized output

--- bin/diff.js
+++ bin/diff.js
@@ -9,13 +9,18 @@ commander
   .arguments('<oldPath> <newPath>')
   .option('-f, --format <format>', 'changes the output format', 'text')
   .option('-p, --pretty', 'improves readability of certain output formats', false)
+  .option('-c, --color', 'colorizes certain output formats', false)
   .action((oldPath, newPath) => {
     const oldLock = JSON.parse(fs.readFileSync(oldPath));
     const newLock = JSON.parse(fs.readFileSync(newPath));
 
     const changes = diff(oldLock, newLock);
 
-    print(changes, commander.format, commander.pretty);
+    print(changes, {
+      format: commander.format,
+      pretty: commander.pretty,
+      color: commander.color,
+    });
 
     if (Object.keys(changes).length > 0) {
       process.exit(1);
